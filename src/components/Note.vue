7a70a5acba64ba1f0d4af9ae07d986e978df4f32
Samuel Cole <cole.samuel21@gmail.com>
2019-03-17 14:31:03 -0400

    Cleanup Note component

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -84,7 +84,6 @@ export default {
 
   background: #201c19;
   border-radius: 50px;
-  /*box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);*/
   color: white;
 
   font-family: $font-lobster;
