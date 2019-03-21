bdd7cd1e29a100aafd2a2216c540eea1282c7404
Sam <cole.samuel21@gmail.com>
2019-03-21 11:20:02 -0500

    Fix sass warning in Fretboard component

--- src/components/Fretboard.vue
+++ src/components/Fretboard.vue
@@ -167,7 +167,7 @@ export default {
 
   display: flex;
   flex-direction: column;
-  justify-content: start;
+  justify-content: flex-start;
   align-items: stretch;
   padding: $note-padding--vertical 0;
 }
