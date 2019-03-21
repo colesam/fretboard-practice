fc916506ed2fd1a8dc7f9cadc4659612fb409892
Sam <cole.samuel21@gmail.com>
2019-03-21 12:09:36 -0500

    Add Card display component

---
+++ src/testViews/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <base-layout>
+    <template v-slot:content>
+      <v-layout>
+        <v-flex xs6>
+          <card title="Testing Card">Test 123</card>
+        </v-flex>
+      </v-layout>
+    </template>
+  </base-layout>
+</template>
+
+<script>
+import BaseLayout from '@/components/BaseLayout'
+import Card from '@/components/display/Card'
+
+export default {
+  components: {
+    BaseLayout,
+    Card
+  }
+}
+</script>
