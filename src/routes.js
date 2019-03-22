30f54ae7e851b8af13dabec7330075d245422096
Sam <cole.samuel21@gmail.com>
2019-03-22 11:30:31 -0500

    Revert "Add Card display component"
    This reverts commit fc916506ed2fd1a8dc7f9cadc4659612fb409892.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 // import Dashboard from '@/views/Dashboard'
 // import MandolinFretboard from '@/test_components/MandolinFretboard'
 import Fretboard from '@/testViews/Fretboard'
-import Card from '@/testViews/Card'
 
 export default [
-  { path: '/testing/fretboard', name: 'testing/fretboard', component: Fretboard },
-  { path: '/testing/card', name: 'testing/fretboard', component: Card }
+  // { path: '/testing/mandolin-fretboard', name: 'mandolin-fretboard', component: MandolinFretboard },
+  // { path: '/', name: 'dashboard', component: Dashboard }
+  { path: '/', name: 'testing/fretboard', component: Fretboard }
 ]
