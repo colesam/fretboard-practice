8c6331bc75a0e0c7299d27acd17955bb699ccfba
Samuel Cole <cole.samuel21@gmail.com>
2019-03-29 19:13:04 -0400

    Add general testing view

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import Dashboard from '@/views/Dashboard'
 import Fretboard from '@/testViews/Fretboard'
+import GeneralTesting from '@/testViews/General'
 
 export default [
   { path: '/', name: 'Dashboard', component: Dashboard },
+  { path: '/testing', name: 'General Testing', component: GeneralTesting },
   { path: '/testing/fretboard', name: 'Fretboard', component: Fretboard }
 ]
---
+++ src/testViews/General.vue
@@ -0,0 +1,26 @@
+<template>
+  <base-layout>
+    <template v-slot:content>
+      Test
+    </template>
+  </base-layout>
+</template>
+
+<script>
+import { NOTES } from '@/globals'
+import BaseLayout from '@/components/BaseLayout'
+
+export default {
+  components: {
+    BaseLayout
+  },
+
+  data() {
+    return {
+      NOTES
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
