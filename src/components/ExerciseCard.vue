96cb710dd93bee918f84140a21cd758ad9f7dfab
Samuel Cole <cole.samuel21@gmail.com>
2019-03-30 10:19:52 -0400

    Change ExerciseCard styling

--- src/components/ExerciseCard.vue
+++ src/components/ExerciseCard.vue
@@ -1,11 +1,11 @@
 <template>
   <v-card class="exerciseCard">
-    <v-card-title class="exerciseCard__name">{{ exercise.name }}</v-card-title>
+    <v-card-title class="exerciseCard__name title">{{ exercise.name }}</v-card-title>
     <v-card-text v-if="exerciseHasDescription" class="exerciseCard__desc">
       {{ exercise.description }}
     </v-card-text>
     <v-card-actions v-else class="px-3">
-      <v-btn small class="exerciseCard__createDesc" color="success">Create Description</v-btn>
+      <v-btn small flat class="exerciseCard__createDesc" color="primary">Add Description</v-btn>
     </v-card-actions>
   </v-card>
 </template>
